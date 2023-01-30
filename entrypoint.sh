be92bcf7cf47a45506e51a50d27a4dd6c7b8fb88
Debabrata1980 <105878261+Debabrata1980@users.noreply.github.com>
2023-01-30 22:47:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ secret-id="$1"
 lambda="$2"
 cron_expr="$3"
 
+export AWS_ACCESS_KEY_ID=AKIAT7NSVCSAWAPRXGRN
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION=us-east-1
+
 aws sts get-caller-identity --query "Account" --output text
 
 echo "Secret Rotation started.."
