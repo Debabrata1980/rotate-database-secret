60b459022f5035a5cbf6b738c71ac83c1d92fb7d
debabrata-bhattacharjee <debabrata.bhattacharjee@hp.com>
2023-01-31 11:05:02 +0530

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,14 +5,11 @@ secret-id="$1"
 lambda="$2"
 cron_expr="$3"
 
-export AWS_ACCESS_KEY_ID=AKIAT7NSVCSAWAPRXGRN
-export AWS_SECRET_ACCESS_KEY=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
-export AWS_DEFAULT_REGION=us-east-1
 
 aws sts get-caller-identity --query "Account" --output text
 
 echo "Secret Rotation started.."
-aws secretsmanager rotate-secret --secret-id ${secret-id} --rotation-lambda-arn ${lambda} --rotate-immediately
+aws secretsmanager rotate-secret --secret-id ${secret-id} --rotation-lambda-arn ${lambda} --rotate-immediately --region 'us-east-1'
 	
 #    --rotation-rules "{\"ScheduleExpression\": \"cron(0 16 1,15 * ? *)\", \"Duration\": \"2h\"}"
 	
